2a59dbf976d4657ce36479e9144b96f412f94dd6
vivax3794 <github@vivax3794.com>
2024-04-22 20:39:05 +0200

    update example project

--- sample_project/src/main.rs
+++ sample_project/src/main.rs
@@ -1,75 +1,63 @@
 use std::sync::Arc;
 
-use aniy::{animations, objects, Color};
+use aniy::{animations, objects, objects::Object, Color};
 
 const RED: Color = Color::rgb(200, 0, 0);
 const BLUE: Color = Color::rgb(0, 0, 200);
+const GRAY: Color = Color::rgb(100, 100, 100);
 
 fn main() {
     env_logger::init();
 
     let mut app = aniy::Renderer::new(1920, 1080);
-    app.set_fps(30);
+    app.set_fps(60);
+
+    let hello_world_obj = Arc::new(
+        objects::Text::new("Hello World!")
+            .size(200.0)
+            .anchor("middle"),
+    );
+
+    let hello_world_animation =
+        Arc::new(animations::TextWrite::new(&hello_world_obj));
+    let hello_world_animation =
+        animations::AnimationContainer::new(hello_world_animation)
+            .duration(5.0);
+    let mut hello_world_animation = animations::AnimatedObject {
+        object: hello_world_obj.clone(),
+        enter: hello_world_animation.clone(),
+        exit: hello_world_animation.reverse().duration(2.0),
+    };
+
+    let made_by_obj = Arc::new(
+        objects::Text::new(
+            "Made using custom animation library (by Viv)!",
+        )
+        .size(50.0)
+        .color(GRAY)
+        .anchor("middle")
+        .besides(&hello_world_obj, objects::Direction::Down),
+    );
+    let made_by_anim =
+        Arc::new(animations::TextType(made_by_obj.clone()));
+    let made_by_anim =
+        animations::AnimationContainer::new(made_by_anim)
+            .duration(1.5);
+    let mut made_by_anim = animations::AnimatedObject {
+        object: made_by_obj.clone(),
+        enter: made_by_anim.clone(),
+        exit: made_by_anim.reverse(),
+    };
+
+    made_by_anim.enter =
+        made_by_anim.enter.after(&hello_world_animation.enter);
+    made_by_anim = made_by_anim.lifetime(2.0);
+    hello_world_animation.exit =
+        hello_world_animation.exit.after(&made_by_anim.exit);
 
     let timeline = app.timeline();
+    timeline.add_animation(hello_world_animation);
+    timeline.add_animation(made_by_anim);
 
-    let square_red = Arc::new(
-        objects::Polygon::new([
-            (-200.0, -200.0),
-            (200.0, -200.0),
-            (200.0, 200.0),
-            (-200.0, 200.0),
-        ])
-        .fill(RED)
-        .outline(RED.darken(0.5)),
-    );
-    let square_red_anim = Arc::new(
-        animations::AnimatedObject {
-            object: square_red.clone(),
-            exit: animations::AnimationContainer::new(Arc::new(
-                animations::NoAnimation,
-            ))
-            .duration(0.0),
-            enter: animations::AnimationContainer::new(Arc::new(
-                animations::PolygonDraw(Arc::clone(&square_red)),
-            ))
-            .duration(1.5),
-        }
-        .lifetime(1.0),
-    );
-    timeline.add_animation(square_red_anim.clone());
-
-    let blue_square = Arc::new(
-        objects::Polygon::new([
-            (-300.0, -150.0),
-            (400.0, -70.0),
-            (230.0, 140.0),
-            (-200.0, 200.0),
-        ])
-        .shift(100.0, -200.0)
-        .fill(BLUE)
-        .outline(BLUE.darken(0.5)),
-    );
-    let blue_square_anim = Arc::new(
-        animations::AnimatedObject {
-            object: blue_square.clone(),
-            exit: animations::AnimationContainer::new(Arc::new(
-                animations::PolygonDraw(blue_square.clone()),
-            ))
-            .reverse()
-            .duration(1.5),
-            enter: animations::AnimationContainer::new(Arc::new(
-                animations::PolygonMorph(
-                    square_red.clone(),
-                    blue_square.clone(),
-                ),
-            ))
-            .duration(1.5)
-            .after(&square_red_anim.exit),
-        }
-        .lifetime(1.0),
-    );
-    timeline.add_animation(blue_square_anim);
-
-    app.render().show();
+    app.render();
 }
